a4f5fb1b217ea72d5145a71513b75a3f871c4121
Idar Vassdal <idarv@mesan.no>
2017-04-20 18:37:38 +0200

    :scroll: reformat code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-import { Actions, Scene, Router, Tab } from 'react-native-router-flux';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import { TabViewAnimated, TabBar } from 'react-native-tab-view';
+import React, {Component} from 'react';
+import {Actions, Scene, Router, Tab} from 'react-native-router-flux';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {TabViewAnimated, TabBar} from 'react-native-tab-view';
 import TabView from './views/TabView'
 
 import rootReducer from './reducers';
@@ -10,13 +10,13 @@ import rootReducer from './reducers';
 const store = createStore(rootReducer);
 
 export default class LunsjListeApp extends Component {
-  render() {
-    return (
-      <Provider store={store}>
-        <Router>
-            <Scene key="TabView" component={TabView}/>
-        </Router>
-      </Provider>
-    );
-  }
+    render() {
+        return (
+            <Provider store={store}>
+                <Router>
+                    <Scene key="TabView" component={TabView}/>
+                </Router>
+            </Provider>
+        );
+    }
 }
