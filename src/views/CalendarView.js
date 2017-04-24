6a0af40216a311fcf9289e75de54b74fcb1a3e17
Idar Vassdal <idarv@mesan.no>
2017-04-24 19:53:15 +0200

    :sake: fixed unwanted tab change when changing month

--- src/views/CalendarView.js
+++ src/views/CalendarView.js
@@ -1,29 +1,32 @@
-import React, { Component } from 'react';
-import { Dimensions, StyleSheet } from 'react-native';
-import { Container, Content, Header } from 'native-base';
+import React, {Component} from 'react';
+import {Dimensions} from 'react-native';
+import {Container, Content} from 'native-base';
 import CalendarPicker from 'react-native-calendar-picker';
 import {Actions} from 'react-native-router-flux'
 
 export default class CalendarView extends Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.handleDateChange = this.handleDateChange.bind(this);
     }
 
     handleDateChange(date) {
+        // Only change tab if a day is changed, not if a month is changed
+        if (date.getMonth() === this.props.selectedDate.getMonth()) {
+            this.props.setNavigationStateIndex(1);
+        }
         this.props.setSelectedDate(date);
-        this.props.setNavigationStateIndex(1);
     }
 
     render() {
         return (
             <Container>
                 <Content>
-                    <CalendarPicker 
+                    <CalendarPicker
                         selectedDate={this.props.selectedDate}
                         screenWidth={Dimensions.get('window').width}
                         onDateChange={date => this.handleDateChange(date)}
-                        selectedDayColor={'#5ce6F0'} />
+                        selectedDayColor={'#5ce6F0'}/>
                 </Content>
             </Container>
         )
