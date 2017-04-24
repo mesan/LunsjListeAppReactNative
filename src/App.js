c32726560fd180dd346dd6788b380299653d94c9
Idar Vassdal <idarv@mesan.no>
2017-04-24 18:17:04 +0200

    merge from master

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React, { Component } from 'react';
-import { Scene, Router } from 'react-native-router-flux';
 import { applyMiddleware, createStore } from 'redux';
-import { middleware as reduxPackMiddleware } from 'redux-pack'
+import { middleware as reduxPackMiddleware } from 'redux-pack';
 import createLogger from 'redux-logger';
 import { Provider } from 'react-redux';
-import React, {Component} from 'react';
 import {Actions, Scene, Router, Tab} from 'react-native-router-flux';
-import {createStore} from 'redux';
-import {Provider} from 'react-redux';
 import {TabViewAnimated, TabBar} from 'react-native-tab-view';
-import TabContainer from './containers/TabContainer'
-
+import TabContainer from './containers/TabContainer';
 import rootReducer from './reducers';
-import Calendar from './containers/Calendar';
-import Home from './containers/Home';
-import Login from './containers/Login';
-
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
 
 const logger = createLogger();
 const store = createStore(rootReducer, applyMiddleware(logger, reduxPackMiddleware));
